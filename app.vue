0e6dca0ec434eff485bfe3dc8f3ca3f726cc56e8
hammas-19 <hammasmasood@gmail.com>
2024-01-17 17:50:11 +0500

    initial commit for setup

---
+++ app.vue
@@ -0,0 +1,8 @@
+<template>
+  <div class="text-green-400 w-full border-4 rounded-md h-dvh bg-gray-400">
+       <span v-for="items in 30" class="size-40 bg-yellow-100 border-2 border-red-400 text-4xl font-semibold p-10">
+         Nuxt-Tailwind BoilerPlate
+       </span>
+  </div>
+
+</template>
